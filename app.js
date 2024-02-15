25c425ce4232eb41e871689028f34932804724a2
azamattt <sabdenbek.4@gmail.com>
2024-02-15 22:02:55 +0600

    trying vercel

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
-
 const authorsRouter = require("./routes/authorsRouter");
 const genresRouter = require("./routes/genresRouter");
 const booksRouter = require("./routes/booksRouter");
 app.use(express.json());
-
+const port = process.env.PORT;
 app.use("/authors", authorsRouter);
 app.use("/genres", genresRouter);
 app.use("/books", booksRouter);
+app.get("/", (req, res) => {
+  res.json("Hello Page");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
